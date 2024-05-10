2da6b910929a506080a1095fc8dbb511b65a7c18
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-10 12:42:54 +0530

    if else condition

---
+++ src/unit/Unit.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unit;
+
+/**
+ *
+ * @author dell1
+ */
+public class Unit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        int a = 6,b=7,c=3;
+        if (a>b && b>c) {                //6>7 && 7>3
+            
+            System.out.println("a");
+        } 
+        else
+        {
+            if(b<c)                        //7<3
+            {
+            System.out.println("b");
+            }
+            else{
+                System.out.println("c");
+                
+            }
+        }
+    }
+}
+
+    
+    
